03a40467760c9ce558fbcacb282ede1cd2ff3c79
Renato Lerac <renatolerac@gmail.com>
2012-12-12 12:16:10 -0200

    Finish layout and routes

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,3 @@
-module ApplicationHelper
-
-# Returns the full title on a per-page basis.
 def full_title(page_title)
 	base_title = "Ruby on Rails Tutorial Sample App"
 	if page_title.empty?
@@ -8,6 +5,4 @@ def full_title(page_title)
 	else
 		"#{base_title} | #{page_title}"
 	end
-end
-
 end

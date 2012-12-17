2f71f101c0eca7ccf8abd68270896eae5f3c8707
Renato Lerac <renatolerac@gmail.com>
2012-12-17 11:45:25 -0200

    Implement chapter 10 exercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,75 +2,122 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	subject { page }
+  subject { page }
 
-	shared_examples_for "all static pages" do
-		it { should have_selector('h1', text: heading) }
-		it { should have_selector('title', text: full_title(page_title)) }
-	end
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
 
-	describe "Home page" do
-		before { visit root_path }
-		let(:heading) { 'Sample App' }
-		let(:page_title) { '' }
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
 
-		it_should_behave_like "all static pages"
-		it { should_not have_selector 'title', text: '| Home' }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Home' }
 
-		describe "for signed-in users" do
-			let(:user) { FactoryGirl.create(:user) }
-			before do
-			FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-			FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-				sign_in user
-				visit root_path
-			end
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
 
-			it "should render the user's feed" do
-				user.feed.each do |item|
-					page.should have_selector("li##{item.id}", text: item.content)
-				end
-			end
-		end
-	end
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
 
-	describe "Help page" do
-		before { visit help_path }
-		let(:heading) { 'Help' }
-		let(:page_title) { 'Help' }
+      describe "micropost feed" do
+        before do
+          30.times do
+            FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+          end
+          visit root_path
+        end
 
-		it_should_behave_like "all static pages"
-	end
+        describe "pagination" do
 
-	describe "About page" do
-		before { visit about_path }
-		let(:heading) { 'About Us' }
-		let(:page_title) { 'About' }
+          it { should have_selector('div.pagination') }
 
-		it_should_behave_like "all static pages"
-	end
+          it "should list each micropost" do
+            user.microposts.paginate(page: 1).each do |micropost|
+              page.should have_selector('li', text: micropost.content)
+            end
+          end
+        end
+      end
 
-	describe "Contact page" do
-		before { visit contact_path }
-		let(:heading) { 'Contact' }
-		let(:page_title) { 'Contact' }
+      describe "micropost sidebar" do
 
-		it_should_behave_like "all static pages"
-	end
+        describe "micropost counts" do
 
-	it "should have the right links on the layout" do
-		visit root_path
-		click_link "About"
-		page.should have_selector 'title', text: full_title('About Us')
-		click_link "Help"
-		page.should have_selector 'title', text: full_title('Help')
-		click_link "Contact"
-		page.should have_selector 'title', text: full_title('Contact')
-		click_link "Home"
-		click_link "Sign up now!"
-		page.should have_selector 'title', text: full_title('Sign up')
-		click_link "sample app"
-		page.should have_selector 'title', text: full_title('')
-	end
+          describe "when there are many feeds" do
+            it { should have_content("#{user.microposts.count} microposts") }
+          end
+
+          describe "when there is only one feed" do
+            before do 
+              (user.microposts.count - 1 ).times { click_link("delete") }
+            end
+
+            it { should have_content("1 micropost") }
+            it { should_not have_content("1 microposts") }
+          end
+
+          describe "when there are no feeds" do
+            before do 
+              user.microposts.count.times { click_link("delete") }
+            end
+
+            it { should have_content("0 microposts") }
+          end
+        end
+      end
+    end
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "About page" do
+    before { visit about_path }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
+  end
 
 end
